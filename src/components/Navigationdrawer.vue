1f538ba3ddb91e27e950fa44aa4c830b7e4dd2e3
DerikCaceres <derik.maganhato@hotmail.com>
2021-11-28 10:41:05 -0300

    commit 3

---
+++ src/components/Navigationdrawer.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-navigation-drawer clipped permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+           Derik Caceres 
+          </v-list-item-title>
+          <v-list-item-subtitle>
+           RA - 190695
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<style  scoped>
+    .mx-auto {
+        flex: 1;
+    }
+</style>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
